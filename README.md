a3652f00c54761b9c3d2c4bdd85f57e739f1a691
Eugene Matveyev <evgeniy.matveev@ihsmarkit.com>
2018-02-19 22:34:08 +0200

    final 0.1.0

---
+++ README.md
@@ -0,0 +1,16 @@
+zeppelin-aggrid
+===============
+
+Data visualization for [Apache Zeppelin](http://zeppelin.apache.org) using [Ag-Grid](https://www.ag-grid.com).
+
+
+## How to use
+
+Place [zeppelin-aggrid.json](https://github.com/menix/zeppelin-aggrid/blob/master/zeppelin-aggrid.json) in your local registry (default location is `ZEPPELIN_HOME/helium`.
+
+And enable visualization from the Helium page. You can go there from dropdown menu under your username at the top right corner in Zeppelin.
+
+
+## Compatibility
+
+Requires Zeppelin 0.7.2+
--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-import Visualization from '../../visualization';
-import PassthroughTransformation from '../../../tabledata/passthrough';
+import Visualization from 'zeppelin-vis';
+import PassthroughTransformation from 'zeppelin-tabledata/passthrough';
+import agGrid from 'ag-grid/dist/ag-grid';
 
-import agGrid from '../../../../../bower_components/ag-grid/dist/ag-grid';
-//import '../../../../../bower_components/ag-grid/dist/styles/ag-grid.css';
-//import '../../../../../bower_components/ag-grid/dist/styles/theme-fresh.css';
-
-/**
- * Visualize data in bar char
- */
 export default class AgGridTable extends Visualization {
   constructor(targetEl, config) {
     super(targetEl, config);
@@ -15,27 +9,10 @@ export default class AgGridTable extends Visualization {
     this.passthrough = new PassthroughTransformation(config);
     this.targetEl = targetEl[0];
     this.targetEl.classList.add('ag-fresh');
-    // specify the columns
-    this.columnDefs = [
-      {headerName: "Make", field: "make"},
-      {headerName: "Model", field: "model"},
-      {headerName: "Price", field: "price"}
-    ];
 
-    // specify the data
-    this.rowData = [
-      {make: "Toyota", model: "Celica", price: 35000},
-      {make: "Ford", model: "Mondeo", price: 32000},
-      {make: "Porsche", model: "Boxter", price: 72000}
-    ];
-
-    // let the grid know which columns and what data to use
     this.gridOptions = {
-      columnDefs: null,
-      rowData: null,
       enableSorting: true,
-      enableFilter: true,
-      //onGridReady: () => console.log('gridOptions!!!', this)
+      enableFilter: true
     };
   };
 
@@ -48,32 +25,21 @@ export default class AgGridTable extends Visualization {
   };
 
   render(data) {
-    console.log("agggggrid", data, this, JSON.stringify(this.gridOptions));
-
     let columnDefs = data.columns.map(col => {
       return {'headerName': col.name, 'field': col.name.toLowerCase()}
     });
     let rowData = data.rows.map(row => {
-        return {
-          [columnDefs[0].field]: row[0], 
-          [columnDefs[1].field]: row[1], 
-          [columnDefs[2].field]: row[2]
-        }
+      let obj = {};
+      columnDefs.forEach((col, idx) => {
+        obj[col.field] = row[idx];
       });
+      return obj;
+    });
+    
     this.gridOptions.columnDefs = columnDefs;
     this.gridOptions.rowData = rowData;
 
-    console.log('columnDefs', columnDefs);
-    // create the grid passing in the div to use together with the columns & data we want to use
     new agGrid.Grid(this.targetEl, this.gridOptions);
-  };
-
-  /**
-   * Set new config
-   */
-  setConfig(config) {
-    super.setConfig(config);
-    this.pivot.setConfig(config);
   };
 
 }
